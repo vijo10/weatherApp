238ed8d2fe6423c90590938614dbf55e3b680acb
VIJO LOUIS <vijolouis11@gmail.com>
2022-11-28 22:31:23 +0530

    changed settings

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -87,14 +87,10 @@ WSGI_APPLICATION = 'weatherApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'weatherApp',
-        'USER': 'postgres', 
-        'PASSWORD': 'Vijolouis@011',
-        'HOST': '127.0.0.1',
-        'PORT': '5432', 
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
 
 
