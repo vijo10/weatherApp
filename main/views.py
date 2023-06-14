3a86132edf6df158b0e008dd48efe32b4965713e
VIJO LOUIS <vijolouis11@gmail.com>
2023-06-14 18:01:04 +0530

    ui change

--- main/views.py
+++ main/views.py
@@ -1,38 +1,45 @@
 import json
-from math import ceil
-from django.shortcuts import redirect, render  
-import urllib.request  
+from django.shortcuts import render
+import urllib.request
 from django.conf import settings
+from django.contrib import messages
 
-apid=settings.APID
-   
-# Create your views here.  
-  
-def home(request):  
-    if request.method == 'POST':  
-        # Get the city name from the user api = http://api.openweathermap.org/data/2.5/weather  
+apid = settings.APID
+
+# Create your views here.
+import urllib.error
+
+
+def home(request):
+    context = {}
+
+    if request.method == 'POST':
+        # Get the city name from the user
         city = request.POST.get('city', 'True')
-        city=city.replace(" ","+")
+        city = city.replace(" ", "+")
 
-          
-        # retreive the information using api  
-        source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apid}').read()  
-        # convert json data file into python dictionary  
-        list_of_data = json.loads(source)  
-        print(list_of_data)  
-        # create dictionary and convert value in string 
+        try:
+            # Retrieve the information using API
+            source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apid}').read()
+            # Convert JSON data file into Python dictionary
+            list_of_data = json.loads(source)
 
-        temp=list_of_data['main']['temp']
-        tem=ceil((temp-32)*5/9) 
-        context = {
-            'city': city.replace("+"," "),  
-            "country_code": str(list_of_data['sys']['country']), 
-            "sunset":str(list_of_data['sys']['sunset']), 
-            "temp": str(tem),  
-            "description":str(list_of_data['weather'][0]['description']),
-            "icon":str(list_of_data['weather'][0]['icon'])  
-        }
-    else:
-        context={}
-    # send dictionary to the index.html  
-    return render(request, 'home.html', context)  
+            temp = list_of_data['main']['temp']
+            tem = round((temp - 32) * 5 / 9)
+            context = {
+                'city': city.replace("+", " "),
+                "country_code": str(list_of_data['sys']['country']),
+                "temp": str(tem),
+                "description": str(list_of_data['weather'][0]['description']),
+                "icon": str(list_of_data['weather'][0]['icon'])
+            }
+        except urllib.error.HTTPError as e:
+            # If HTTPError 404 occurs, set error message in context dictionary
+            if e.code == 404:
+                messages.error(request, f"{city.replace('+', ' ')} not found. Please enter a valid city name.")
+            else:
+                # If a different HTTP error occurs, re-raise the exception
+                raise e
+
+    # Send dictionary to the index.html
+    return render(request, 'home.html', context)
